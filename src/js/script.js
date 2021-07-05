c55b4788acef017fc42d4ad31d09d0bf7f99f1c0
peamexx <peamexx@gmail.com>
2021-07-05 20:22:49 +0900

    [ADD] 서비스 소개, 문의하기 제작

--- src/js/script.js
+++ src/js/script.js
@@ -1,10 +1,12 @@
 const nav = document.querySelector('nav');
 const footer = document.querySelector('footer');
 const asideAll = document.querySelectorAll('aside');
+const tab = document.querySelector('.how ul');
 
 nav.addEventListener('click', menuOpenClose);
 footer.addEventListener('click', popupOpen);
 asideAll.forEach((item) => item.addEventListener('click', popupClose));
+tab.addEventListener('click', tabToggle);
 
 function menuOpenClose(e) {
     if(e.target.className.includes('menu-open')) {
@@ -28,7 +30,7 @@ function popupOpen(e) {
         document.querySelector('.popup-pp').classList.add('on');
         document.querySelector('.popup-pp .black').classList.add('opacity-on');
     }
-}
+};
 
 function popupClose(e) {
     if(e.target.className.includes('popup-close')) {
@@ -38,4 +40,45 @@ function popupClose(e) {
             aside.querySelector('.black').classList.remove('opacity-on');
         }
     }
-}
+};
+
+function tabToggle(e) {
+    let dataset = e.target.dataset.sort;
+    resizeHeight(dataset);
+
+    // 탭 메뉴 toggle
+    document.querySelectorAll('.how ul li').forEach((item) => {
+        item.classList.remove('tab-on');
+    });
+    e.target.classList.add('tab-on');
+
+    // 해당 탭 내용 toggle
+    document.querySelectorAll('.how .contents > div').forEach((item) => {
+        if(item.className.includes(dataset)) {
+            item.classList.remove('contents-off');
+            item.classList.add('contents-on');
+        } else {
+            item.classList.remove('contents-on');
+            item.classList.add('contents-off');
+        }
+    });
+};
+
+function resizeHeight(datasetName) {
+    document.querySelectorAll('.how .contents > div').forEach((item) => {
+        if(item.className.includes(datasetName)) {
+            document.querySelector('.how .contents').style.height = `${item.offsetHeight}px`;
+        }
+    });
+};
+
+// slick
+$(document).ready(function() {
+    $('.title-slider').slick({
+        dots: true,
+        arrows: false,
+        autoplay: true,
+        autoplaySpeed: 2000,
+        responsive: true
+    });
+})
